MiyamotoSatoshi/vuex

commit be5ff3968b4f3cadf97e9a0713eb0decf2665ef3

Focus v-select's internal input when focus() is called (#1821)
* Focus v-select's internal input on focus

Fixes #1819

* Don't focus this.$el if it's an autocomplete

src/components/VSelect/VSelect.js
@@ -248,7 +248,7 @@ export default {
     },
     isBooted () {
       this.$nextTick(() => {
-        if (this.content) {
+        if (this.content instanceof EventTarget) {
           this.content.addEventListener('scroll', this.onScroll, false)
         }
       })
@@ -394,9 +394,9 @@ export default {
       this.isFocused = true
 
       if (this.$refs.input && this.isAutocomplete) {
-        this.$nextTick(() => {
-          // this.$refs.input.focus()
-        })
+        this.$refs.input.focus()
+      } else {
+        this.$el.focus()
       }
 
       this.$emit('focus')
@@ -423,7 +423,7 @@ export default {
 
           !this.isFocused && this.focus()
         },
-        keydown: this.onKeyDown // Located in mixins/autocomplete.js
+        keydown: this.onKeyDown // Located in mixins/select-autocomplete.js
       }
     },
     genLabel () {

commit 047ffb10a82428fd2a5828911a534443c7dfd33c

fix(VSelect): check for addEventListener in isBooted watcher
Turns out IE11 doesn't know what an EventTarget is

Fixes #2213
Regression from #1821

src/components/VSelect/VSelect.js
@@ -266,7 +266,7 @@ export default {
     },
     isBooted () {
       this.$nextTick(() => {
-        if (this.content instanceof EventTarget) {
+        if (this.content && this.content.addEventListener) {
           this.content.addEventListener('scroll', this.onScroll, false)
         }
       })