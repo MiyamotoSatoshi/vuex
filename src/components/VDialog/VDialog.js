vuetifyjs/vuetify

commit 993bcb33ee3533e7d10d829a9c68c4603f27bbc8

fix(VDialog): don't create overlay on initially open fullscreen dialogs
See https://codepen.io/anon/pen/rGqXoP?editors=1010

src/components/VDialog/VDialog.js
@@ -76,12 +76,12 @@ export default {
   watch: {
     isActive (val) {
       if (val) {
-        !this.fullscreen && !this.hideOverlay && this.genOverlay()
-        this.fullscreen && this.hideScroll()
-        this.$refs.content.focus()
+        this.init()
       } else {
         if (!this.fullscreen) this.removeOverlay()
         else this.showScroll()
+
+        this.unbind()
       }
     }
   },
@@ -102,7 +102,9 @@ export default {
       return !this.persistent && getZIndex(this.$refs.content) >= this.getMaxZIndex()
     },
     init () {
-      this.isActive && this.genOverlay()
+      !this.fullscreen && !this.hideOverlay && this.genOverlay()
+      this.fullscreen && this.hideScroll()
+      this.$refs.content.focus()
 
       if (this.$listeners.keydown) this.bind()
     },

MiyamotoSatoshi/vuex

commit 53ef8527fb22f8150e260e32a65e29c589046ee2

fix(VDialog): only disable scrolling when active
Regression from 993bcb33ee3533e7d10d829a9c68c4603f27bbc8

Fixes #2262

src/components/VDialog/VDialog.js
@@ -73,11 +73,9 @@ export default {
   watch: {
     isActive (val) {
       if (val) {
-        this.init()
+        this.show()
       } else {
-        if (!this.fullscreen) this.removeOverlay()
-        else this.showScroll()
-
+        this.removeOverlay()
         this.unbind()
       }
     }
@@ -99,11 +97,13 @@ export default {
       return !this.persistent && getZIndex(this.$refs.content) >= this.getMaxZIndex()
     },
     init () {
+      this.isActive && this.show()
+    },
+    show () {
       !this.fullscreen && !this.hideOverlay && this.genOverlay()
       this.fullscreen && this.hideScroll()
       this.$refs.content.focus()
-
-      if (this.$listeners.keydown) this.bind()
+      this.$listeners.keydown && this.bind()
     },
     bind () {
       window.addEventListener('keydown', this.onKeydown)