vuetifyjs/vuetify

commit ec9fcae39d982eddb0b51f866684eb85682c8ba8

fix: do not apply ripple on disabled routable (#2899)

src/mixins/routable.js
@@ -34,7 +34,7 @@ export default {
         props: {},
         directives: [{
           name: 'ripple',
-          value: this.ripple || false
+          value: (this.ripple && !this.disabled) || false
         }],
         on: {
           ...(this.$listeners || {}),

src/components/VDataIterator/__snapshots__/VDataIterator.spec.js.snap
@@ -105,7 +105,7 @@ exports[`VDataIterator.js should match a snapshot - footer slot 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Previous page"
         >
           <div class="btn__content">
@@ -119,7 +119,7 @@ exports[`VDataIterator.js should match a snapshot - footer slot 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Next page"
         >
           <div class="btn__content">
@@ -253,7 +253,7 @@ exports[`VDataIterator.js should match a snapshot - no data 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Previous page"
         >
           <div class="btn__content">
@@ -267,7 +267,7 @@ exports[`VDataIterator.js should match a snapshot - no data 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Next page"
         >
           <div class="btn__content">
@@ -392,7 +392,7 @@ exports[`VDataIterator.js should match a snapshot - no matching records 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Previous page"
         >
           <div class="btn__content">
@@ -406,7 +406,7 @@ exports[`VDataIterator.js should match a snapshot - no matching records 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Next page"
         >
           <div class="btn__content">
@@ -535,7 +535,7 @@ exports[`VDataIterator.js should match a snapshot - with data 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Previous page"
         >
           <div class="btn__content">
@@ -549,7 +549,7 @@ exports[`VDataIterator.js should match a snapshot - with data 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Next page"
         >
           <div class="btn__content">

src/components/VDataTable/__snapshots__/VDataTable.spec.js.snap
@@ -161,7 +161,7 @@ exports[`VDataTable.vue should match a snapshot - no data 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Previous page"
         >
           <div class="btn__content">
@@ -175,7 +175,7 @@ exports[`VDataTable.vue should match a snapshot - no data 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Next page"
         >
           <div class="btn__content">
@@ -354,7 +354,7 @@ exports[`VDataTable.vue should match a snapshot - no matching results 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Previous page"
         >
           <div class="btn__content">
@@ -368,7 +368,7 @@ exports[`VDataTable.vue should match a snapshot - no matching results 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Next page"
         >
           <div class="btn__content">
@@ -549,7 +549,7 @@ exports[`VDataTable.vue should match a snapshot - with data 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Previous page"
         >
           <div class="btn__content">
@@ -563,7 +563,7 @@ exports[`VDataTable.vue should match a snapshot - with data 1`] = `
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Next page"
         >
           <div class="btn__content">
@@ -647,7 +647,7 @@ exports[`VDataTable.vue should match a snapshot with single rows-per-page-items 
         <button disabled="disabled"
                 type="button"
                 class="btn btn--disabled btn--flat btn--icon"
-                data-ripple="true"
+                data-ripple="false"
                 aria-label="Previous page"
         >
           <div class="btn__content">

MiyamotoSatoshi/vuex

commit 32a77ce6ee6b84b9613976084c705480c0b2ef2a

fix: ripple prop as an object is passed as a bool to ripple directive
broken by ec9fcae39d982eddb0b51f866684eb85682c8ba8

src/mixins/routable.js
@@ -34,7 +34,7 @@ export default {
         props: {},
         directives: [{
           name: 'ripple',
-          value: (this.ripple && !this.disabled) || false
+          value: (this.ripple && !this.disabled) ? this.ripple : false
         }],
         on: {
           ...(this.$listeners || {}),