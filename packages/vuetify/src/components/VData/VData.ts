MiyamotoSatoshi/vuex

commit 49198ec0af470a52a6e20bdca112d4ebf79e855c

fix(VDataTable): clamp current page when filtering (#13087)
fixes #12732

packages/vuetify/src/components/VData/VData.ts
@@ -365,7 +365,7 @@ export default Vue.extend({
       // Make sure we don't try to display non-existant page if items suddenly change
       // TODO: Could possibly move this to pageStart/pageStop?
       if (this.serverItemsLength === -1 && items.length <= this.pageStart) {
-        this.internalOptions.page = Math.max(1, this.internalOptions.page - 1)
+        this.internalOptions.page = Math.max(1, Math.ceil(items.length / this.internalOptions.itemsPerPage))
       }
 
       return items.slice(this.pageStart, this.pageStop)

commit 1d497ef7014a40a3f5423c1a21399c09009ddfcd

fix(VDataTable): prevent NaN when itemsPerPage is 0
regression from #13087

packages/vuetify/src/components/VData/VData.ts
@@ -365,7 +365,7 @@ export default Vue.extend({
       // Make sure we don't try to display non-existant page if items suddenly change
       // TODO: Could possibly move this to pageStart/pageStop?
       if (this.serverItemsLength === -1 && items.length <= this.pageStart) {
-        this.internalOptions.page = Math.max(1, Math.ceil(items.length / this.internalOptions.itemsPerPage))
+        this.internalOptions.page = Math.max(1, Math.ceil(items.length / this.internalOptions.itemsPerPage)) || 1 // Prevent NaN
       }
 
       return items.slice(this.pageStart, this.pageStop)