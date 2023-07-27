vuetifyjs/vuetify

commit e99f6fd25ccc19970f63805f8d0cdda96d036255

fix(VSelect): support browser autofill
fixes #5936

packages/vuetify/src/components/VAutocomplete/VAutocomplete.tsx
@@ -278,6 +278,15 @@ export const VAutocomplete = genericComponent<new <
       search.value = (e.target as HTMLInputElement).value
     }
 
+    function onChange (e: Event) {
+      if (vTextFieldRef.value?.matches(':autofill') || vTextFieldRef.value?.matches(':-webkit-autofill')) {
+        const item = items.value.find(item => item.title === (e.target as HTMLInputElement).value)
+        if (item) {
+          select(item)
+        }
+      }
+    }
+
     function onAfterLeave () {
       if (isFocused.value) {
         isPristine.value = true
@@ -378,6 +387,7 @@ export const VAutocomplete = genericComponent<new <
           validationValue={ model.externalValue }
           dirty={ isDirty }
           onInput={ onInput }
+          onChange={ onChange }
           class={[
             'v-autocomplete',
             `v-autocomplete--${props.multiple ? 'multiple' : 'single'}`,

packages/vuetify/src/components/VSelect/VSelect.tsx
@@ -173,7 +173,7 @@ export const VSelect = genericComponent<new <
       menu.value = !menu.value
     }
     function onKeydown (e: KeyboardEvent) {
-      if (props.readonly || form?.isReadonly.value) return
+      if (!e.key || props.readonly || form?.isReadonly.value) return
 
       if (['Enter', ' ', 'ArrowDown', 'ArrowUp', 'Home', 'End'].includes(e.key)) {
         e.preventDefault()
@@ -245,6 +245,17 @@ export const VSelect = genericComponent<new <
     function onFocusin (e: FocusEvent) {
       isFocused.value = true
     }
+    function onModelUpdate (v: any) {
+      if (v == null) model.value = []
+      else if (vTextFieldRef.value?.matches(':autofill') || vTextFieldRef.value?.matches(':-webkit-autofill')) {
+        const item = items.value.find(item => item.title === v)
+        if (item) {
+          select(item)
+        }
+      } else if (vTextFieldRef.value) {
+        vTextFieldRef.value.value = ''
+      }
+    }
 
     useRender(() => {
       const hasChips = !!(props.chips || slots.chip)
@@ -268,7 +279,7 @@ export const VSelect = genericComponent<new <
           ref={ vTextFieldRef }
           { ...textFieldProps }
           modelValue={ model.value.map(v => v.props.value).join(', ') }
-          onUpdate:modelValue={ v => { if (v == null) model.value = [] } }
+          onUpdate:modelValue={ onModelUpdate }
           v-model:focused={ isFocused.value }
           validationValue={ model.externalValue }
           dirty={ isDirty }
@@ -284,7 +295,7 @@ export const VSelect = genericComponent<new <
             props.class,
           ]}
           style={ props.style }
-          readonly
+          inputmode="none"
           placeholder={ placeholder }
           onClick:clear={ onClear }
           onMousedown:control={ onMousedownControl }

packages/vuetify/src/components/VSelect/VSelect.sass
@@ -22,6 +22,7 @@
         width: 100%
         transition: none
         pointer-events: none
+        caret-color: transparent
 
   .v-field--dirty
     .v-select__selection

packages/vuetify/src/composables/forwardRefs.ts
@@ -58,6 +58,22 @@ export function forwardRefs<T extends {}, U extends Ref<HTMLElement | Omit<Compo
       }
       return false
     },
+    set (target, key, value) {
+      if (Reflect.has(target, key)) {
+        return Reflect.set(target, key, value)
+      }
+
+      // Skip internal properties
+      if (typeof key === 'symbol' || key.startsWith('__')) return false
+
+      for (const ref of refs) {
+        if (ref.value && Reflect.has(ref.value, key)) {
+          return Reflect.set(ref.value, key, value)
+        }
+      }
+
+      return false
+    },
     getOwnPropertyDescriptor (target, key) {
       const descriptor = Reflect.getOwnPropertyDescriptor(target, key)
       if (descriptor) return descriptor

MiyamotoSatoshi/vuex

commit 710488c0e403143dae17c15f5297f85b4aec6a53

fix(VSelect): don't break environments without :autofill selector
regression from e99f6fd25ccc19970f63805f8d0cdda96d036255

packages/vuetify/src/components/VAutocomplete/VAutocomplete.tsx
@@ -25,7 +25,7 @@ import { makeTransitionProps } from '@/composables/transition'
 
 // Utilities
 import { computed, mergeProps, nextTick, ref, shallowRef, watch } from 'vue'
-import { genericComponent, noop, omit, propsFactory, useRender, wrapInArray } from '@/util'
+import { genericComponent, matchesSelector, noop, omit, propsFactory, useRender, wrapInArray } from '@/util'
 
 // Types
 import type { PropType } from 'vue'
@@ -279,7 +279,7 @@ export const VAutocomplete = genericComponent<new <
     }
 
     function onChange (e: Event) {
-      if (vTextFieldRef.value?.matches(':autofill') || vTextFieldRef.value?.matches(':-webkit-autofill')) {
+      if (matchesSelector(vTextFieldRef.value, ':autofill') || matchesSelector(vTextFieldRef.value, ':-webkit-autofill')) {
         const item = items.value.find(item => item.title === (e.target as HTMLInputElement).value)
         if (item) {
           select(item)

packages/vuetify/src/components/VOverlay/useActivator.tsx
@@ -20,9 +20,9 @@ import {
   bindProps,
   getCurrentInstance,
   IN_BROWSER,
+  matchesSelector,
   propsFactory,
   refElement,
-  SUPPORTS_FOCUS_VISIBLE,
   unbindProps,
 } from '@/util'
 
@@ -114,10 +114,7 @@ export function useActivator (
       runCloseDelay()
     },
     onFocus: (e: FocusEvent) => {
-      if (
-        SUPPORTS_FOCUS_VISIBLE &&
-        !(e.target as HTMLElement).matches(':focus-visible')
-      ) return
+      if (matchesSelector(e.target as HTMLElement, ':focus-visible') === false) return
 
       isFocused = true
       e.stopPropagation()

packages/vuetify/src/components/VSelect/VSelect.tsx
@@ -24,7 +24,7 @@ import { makeTransitionProps } from '@/composables/transition'
 
 // Utilities
 import { computed, mergeProps, ref, shallowRef } from 'vue'
-import { deepEqual, genericComponent, omit, propsFactory, useRender, wrapInArray } from '@/util'
+import { deepEqual, genericComponent, matchesSelector, omit, propsFactory, useRender, wrapInArray } from '@/util'
 
 // Types
 import type { Component, PropType } from 'vue'
@@ -247,7 +247,7 @@ export const VSelect = genericComponent<new <
     }
     function onModelUpdate (v: any) {
       if (v == null) model.value = []
-      else if (vTextFieldRef.value?.matches(':autofill') || vTextFieldRef.value?.matches(':-webkit-autofill')) {
+      else if (matchesSelector(vTextFieldRef.value, ':autofill') || matchesSelector(vTextFieldRef.value, ':-webkit-autofill')) {
         const item = items.value.find(item => item.title === v)
         if (item) {
           select(item)

packages/vuetify/src/components/VSelectionControl/VSelectionControl.tsx
@@ -21,8 +21,8 @@ import {
   filterInputAttrs,
   genericComponent,
   getUid,
+  matchesSelector,
   propsFactory,
-  SUPPORTS_FOCUS_VISIBLE,
   useRender,
   wrapInArray,
 } from '@/util'
@@ -168,10 +168,7 @@ export const VSelectionControl = genericComponent<new <T>(
 
     function onFocus (e: FocusEvent) {
       isFocused.value = true
-      if (
-        !SUPPORTS_FOCUS_VISIBLE ||
-        (SUPPORTS_FOCUS_VISIBLE && (e.target as HTMLElement).matches(':focus-visible'))
-      ) {
+      if (matchesSelector(e.target as HTMLElement, ':focus-visible') !== false) {
         isFocusVisible.value = true
       }
     }

packages/vuetify/src/util/globals.ts
@@ -1,7 +1,3 @@
 export const IN_BROWSER = typeof window !== 'undefined'
 export const SUPPORTS_INTERSECTION = IN_BROWSER && 'IntersectionObserver' in window
 export const SUPPORTS_TOUCH = IN_BROWSER && ('ontouchstart' in window || window.navigator.maxTouchPoints > 0)
-export const SUPPORTS_FOCUS_VISIBLE = IN_BROWSER &&
-  typeof CSS !== 'undefined' &&
-  typeof CSS.supports !== 'undefined' &&
-  CSS.supports('selector(:focus-visible)')

packages/vuetify/src/util/helpers.ts
@@ -1,5 +1,6 @@
 // Utilities
 import { camelize, capitalize, computed, Fragment, reactive, toRefs, watchEffect } from 'vue'
+import { IN_BROWSER } from '@/util/globals'
 
 // Types
 import type {
@@ -601,3 +602,19 @@ export function isEmpty (val: any): boolean {
 }
 
 export function noop () {}
+
+/** Returns null if the selector is not supported or we can't check */
+export function matchesSelector (el: Element | undefined, selector: string): boolean | null {
+  const supportsSelector = IN_BROWSER &&
+    typeof CSS !== 'undefined' &&
+    typeof CSS.supports !== 'undefined' &&
+    CSS.supports(`selector(${selector})`)
+
+  if (!supportsSelector) return null
+
+  try {
+    return !!el && el.matches(selector)
+  } catch (err) {
+    return null
+  }
+}